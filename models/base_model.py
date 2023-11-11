ac5c77e274091f4480630fa0beb9bb165a3e194d
Naftali Holland <naftaliholland01@gmail.com>
2023-11-11 09:20:30 +0300

    Added methods for file storage of the objects and an engine to handle file storage

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,8 @@ This module define a BaseModel class.
 """
 import uuid
 from datetime import datetime
-#from models.__init__ import storage
-import models
+from models import storage
+
 
 class BaseModel:
     """
@@ -13,9 +13,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         my_uuid = uuid.uuid4()
         self.id = str(my_uuid)
-        models.storage.new(self)
-        
-        if kwargs:
+
+        if len(kwargs) > 0:
             for key in kwargs.keys():
                 if key != "__class__":
                     kwargs[key] = (
@@ -26,6 +25,7 @@ class BaseModel:
         else:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -40,14 +40,15 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
         Returns a dictionary containing all keys/values
         of __dict__ of the instance
         """
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        return self.__dict__
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 This module holds FileStorage class
 """
 import json
-from models.base_model import BaseModel
+
 
 class FileStorage:
     """
@@ -10,7 +10,6 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
-
 
     def all(self):
         """
@@ -23,56 +22,18 @@ class FileStorage:
         sets in __objects the obj with key <0bj class name>.id
         """
         key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
         """
         Serializes __objects to the JSON file
         """
-        my_dict = {}
-        for k, v in FileStorage.__objects.items():
-            my_dict[k] = v.to_dict()
-
- 
-        with open(FileStorage.__file_path, "w") as file:
-            json.dump(my_dict, file)
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            json.dump(self.__objects, file)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects
-        
         try:
-            with open(FileStorage.__file_path, "r") as file:
-                content = file.read()
-
-            objects = eval(content)
-            for (key, value) in objects.items():
-                objects[key] = eval(key.split('.')[0] + '(**value)')
-            print(type(objects))
-            self.__objects = objects
-
-
+            with open(self.__file_path, "r", encoding="utf-8") as file:
+                self.__objects = json.load(file)
         except FileNotFoundError:
             pass
-
-        """
-
-        def reload(self):
-            try:
-                with open(self.__file_path, 'r', encoding='utf-8') as file:
-                    #my_obj = myFile.read()
-                    objects = json.loads(file)
-
-                    for key, value in objects.items():
-                        if '.' in key:  # Checking if the dot exists in the key
-                            class_name, obj_id = key.split('.')
-                            # Assuming your classes are imported properly, create instances dynamically
-                            if class_name == 'BaseModel':
-                                from models.base_model import BaseModel  # Import the specific class dynamically
-                                obj = BaseModel(**value)  # Create an instance of BaseModel
-                                objects[key] = obj
-
-                    self.__objects = objects
-
-            except FileNotFoundError:
-                pass
